8bca65e8a42bffc6d805bfdbedabff7ca2caacf3
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2023-06-09 12:01:57 +0200

    feat: add contact page and upload image

---
+++ app/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage | My first App Php</title>
+    <link rel="stylesheet" href="/assets/styles/main.css">
+</head>
+
+<body>
+    <?php include_once './templates/header.php'; ?>
+    <main>
+        <section class="container">
+            <form action="/contact.php" method="POST" class="form" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="nom">Votre Nom:</label>
+                    <input type="text" name="nom" id="nom" placeholder="Doe">
+                </div>
+                <div class="form-group">
+                    <label for="prenom">Votre prénom:</label>
+                    <input type="text" name="prenom" id="prenom" placeholder="John">
+                </div>
+                <div class="form-group">
+                    <label for="age">Votre age:</label>
+                    <input type="number" name="age" id="age" placeholder="30">
+                </div>
+                <div class="form-group">
+                    <label for="image">Votre image:</label>
+                    <input type="file" name="image" id="image">
+                </div>
+                <button class="btn btn-primary">Envoyer</button>
+            </form>
+        </section>
+    </main>
+</body>
+
+</html>
